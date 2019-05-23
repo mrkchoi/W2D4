e3c1ae6fb348125b4ee29cbb5b716ece4b5ff39c
mrkchoi <kennethichoi@gmail.com>
2019-05-23 14:06:42 -0700

    Complete #my_min

--- execution_time_differences/execution_time_differences.rb
+++ execution_time_differences/execution_time_differences.rb
@@ -18,15 +18,29 @@
 # First, write a function that compares each element to every other element of the list. Return the element if all other elements in the array are larger.
 # What is the time complexity for this function?
 
-# Phase II
-# Now rewrite the function to iterate through the list just once while keeping track of the minimum. What is the time complexity?
-
 
 
 def my_min(arr)
-  output = nil
-
+  output = arr.first
   
+  (1...arr.length).each do |i|
+    (i+1...arr.length).each do |j|
+      output = arr[j] if arr[j] < output
+    end
+  end
   
   output
 end
+
+# list = [ 0, 3, 5, 4, -5, 10, 1, 90 ]
+# p my_min(list)  # =>  -5
+
+
+# Phase II
+# Now rewrite the function to iterate through the list just once while keeping track of the minimum. What is the time complexity?
+
+def my_min_2(arr)
+  
+end
+
+# list = [ 0, 3, 5, 4, -5, 10, 1, 90 ]
